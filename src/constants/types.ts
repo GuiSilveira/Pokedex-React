261ac66f2f08a08a660e78069ded74cf44ee1f34
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-07-18 20:07:21 -0300

    creating type constant and types svg folder

---
+++ src/constants/types.ts
@@ -0,0 +1,98 @@
+import { ReactComponent as rockType } from "../../../assets/svg/types/Rock.svg";
+import { ReactComponent as ghostType } from "../../../assets/svg/types/Ghost.svg";
+import { ReactComponent as steelType } from "../../../assets/svg/types/Steel.svg";
+import { ReactComponent as waterType } from "../../../assets/svg/types/Water.svg";
+import { ReactComponent as grassType } from "../../../assets/svg/types/Grass.svg";
+import { ReactComponent as psychicType } from "../../../assets/svg/types/Psychic.svg";
+import { ReactComponent as iceType } from "../../../assets/svg/types/Ice.svg";
+import { ReactComponent as darkType } from "../../../assets/svg/types/Dark.svg";
+import { ReactComponent as fairyType } from "../../../assets/svg/types/Fairy.svg";
+import { ReactComponent as normalType } from "../../../assets/svg/types/Normal.svg";
+import { ReactComponent as fightingType } from "../../../assets/svg/types/Fighting.svg";
+import { ReactComponent as flyingType } from "../../../assets/svg/types/Flying.svg";
+import { ReactComponent as poisonType } from "../../../assets/svg/types/Poison.svg";
+import { ReactComponent as groundType } from "../../../assets/svg/types/Ground.svg";
+import { ReactComponent as bugType } from "../../../assets/svg/types/Bug.svg";
+import { ReactComponent as fireType } from "../../../assets/svg/types/Fire.svg";
+import { ReactComponent as eletricType } from "../../../assets/svg/types/Eletric.svg";
+import { ReactComponent as dragonType } from "../../../assets/svg/types/Dragon.svg";
+import { ReactComponent as unknowType } from "../../../assets/svg/types/Unknow.svg";
+
+export const TYPES = {
+  rock: {
+    img: rockType,
+    color: "#B69E31",
+  },
+  ghost: {
+    img: ghostType,
+    color: "#70559B",
+  },
+  steel: {
+    img: steelType,
+    color: "#B7B9D0",
+  },
+  water: {
+    img: waterType,
+    color: "#6493EB",
+  },
+  grass: {
+    img: grassType,
+    color: "#74CB48",
+  },
+  psychic: {
+    img: psychicType,
+    color: "#FB5584",
+  },
+  ice: {
+    img: iceType,
+    color: "#9AD6DF",
+  },
+  dark: {
+    img: darkType,
+    color: "#75574C",
+  },
+  fairy: {
+    img: fairyType,
+    color: "#E69EAC",
+  },
+  normal: {
+    img: normalType,
+    color: "#AAA67F",
+  },
+  fighting: {
+    img: fightingType,
+    color: "#C12239",
+  },
+  flying: {
+    img: flyingType,
+    color: "#A891EC",
+  },
+  poison: {
+    img: poisonType,
+    color: "#A43E9E",
+  },
+  ground: {
+    img: groundType,
+    color: "#DEC16B",
+  },
+  bug: {
+    img: bugType,
+    color: "#A7B723",
+  },
+  fire: {
+    img: fireType,
+    color: "#F57D31",
+  },
+  eletric: {
+    img: eletricType,
+    color: "#F9CF30",
+  },
+  dragon: {
+    img: dragonType,
+    color: "#7037FF",
+  },
+  unknow: {
+    img: unknowType,
+    color: "#666666",
+  },
+};
