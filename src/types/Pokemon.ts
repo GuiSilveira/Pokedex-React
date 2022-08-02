f8e9334f690c175f667553daa8dd5fa53ebc9cf2
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-08-02 11:05:34 -0300

    Ajustando o tipo do Pokemon

--- src/types/Pokemon.ts
+++ src/types/Pokemon.ts
@@ -2,7 +2,6 @@ export type Pokemon = {
   id: number;
   name: string;
   sprites: {
-    front_default: string | null;
     other: { "official-artwork": { front_default: string } };
   };
   types: object;
